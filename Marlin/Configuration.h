c92b5a80d59e431ca3b55a869a4d987406fea9db
Stephan van Veen <vanveen.stephan@gmail.com>
2021-07-24 13:37:39 +0200

    Define bed size and position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1337,14 +1337,14 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
+#define X_BED_SIZE 215
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -2
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + X_MIN_POS
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 200
 //#define I_MIN_POS 0
