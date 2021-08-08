793d9d97ad210a3e0eb3e3c81af52e8d51521640
Stephan van Veen <vanveen.stephan@gmail.com>
2021-08-08 15:42:14 +0200

    Define nozzle to probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1147,7 +1147,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 21, 10, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
